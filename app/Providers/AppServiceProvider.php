2b5ec96fd4cee6f560bdd05f8726c4c2f2733145
Raul Gonzalez <bragr77@gmail.com>
2020-06-03 12:07:56 -0400

    Activación y Configuración del Modulo de Autenticación de Laravel, y configuración del idiomas castellano de laravel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; //para corregir lo errores de las tablas a migrar
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191); //para corregir lo errores de las tablas a migrar
     }
 }
