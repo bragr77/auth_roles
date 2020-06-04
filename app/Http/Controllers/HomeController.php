e8c782028692ada2c67ef4d73be864da4b79552d
Raul Gonzalez <bragr77@gmail.com>
2020-06-04 12:29:08 -0400

    Creamos la migración de la tabla roles, agregamos la clave foranea a la tabla users, y creamos y corremos la seed de la tabla roles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,14 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function user()
+    {
+        return view('auth.user');
+    }
+
+    public function admin()
+    {
+        return view('auth.admin');
+    }
 }
---
+++ resources/views/auth/admin.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">ADMINISTRADOR</div>
+
+                <div class="card-body">
+                     Esta logueado como Administrador!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
